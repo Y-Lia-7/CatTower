e4da9c75d511713979d7af033ee6b47e0838095f
Aleksi Kulmanen <p38557@edu.sasky.fi>
2026-08-20 10:53:18 +0300

    KääröScript
    Lisää KääröScriptin

---
+++ Assets/Scripts/KaaroScript.cs
@@ -0,0 +1,21 @@
+using Unity.VectorGraphics;
+using Unity.XR.Oculus.Input;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KaaroScript : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            LoadMainMenu();
+        }
+        
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
